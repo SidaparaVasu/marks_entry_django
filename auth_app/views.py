251c1c08a530e5b888fbdb0e17b8431dd14d2eb2
Sidapara_Vasu <74949446+SidaparaVasu@users.noreply.github.com>
2022-09-05 02:09:33 +0530

    methods name and url changes

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render, HttpResponse
 from auth_app.forms import RegisterForm  
+from django.contrib import messages
 #from auth_app.models import users
 from . import models
 
 # Create your views here.
-def login(request):
+def loginPage(request):
     return render(request, 'login.html')
 
-def faculty_register(request):
+def registerPage(request):
     return render(request, 'register.html')
 
 def registration(request):
     if request.method == "POST":     
         form = RegisterForm(request.POST or None)  
-        #return HttpResponse(form)
         if form.is_valid():  
-            try:  
-                print("Hello")
-                form.save()  
-                return render(request,'login.html')  
-            except:  
-                pass  
+            form.save()  
+            messages.success(request, "You are registered sucessfully in as {username}!")
+            return render(request,'login.html')  
+        else:
+            messages.error(request, "Registration failed! due to less Aukat of {username}!")
     else:  
         form = RegisterForm()  
     return render(request,'register.html',{'form':form})  
