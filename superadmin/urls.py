64d14507544fcaab26972414d3eef72fadb07fc4
Sidapara_Vasu <74949446+SidaparaVasu@users.noreply.github.com>
2022-09-06 00:53:06 +0530

    Added some page in superadmin application

--- superadmin/urls.py
+++ superadmin/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.indexDashboard, name="indexDashboard")
+    path('', views.indexDashboard, name="indexDashboard"),
+    path('profile', views.profile, name="profile"),
+    path('addInstitute', views.addInstitute, name="addInstitute"),
 ]
--- superadmin/views.py
+++ superadmin/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def indexDashboard(request):
     return render(request, "index.html")
+
+def profile(request):
+    return render(request, "profile.html")
+
+def addInstitute(request):
+    return render(request, "addInstitute.html")
