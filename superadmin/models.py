f0a7b4100f3f48a49f8823f2fc9757efad58a3bd
codeonlife <bhavinkalal3118@gmail.com>
2022-09-07 08:48:23 +0530

    migrate done

--- superadmin/models.py
+++ superadmin/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class addInstitute(models.Model):
-    instituteId = models.TextField(max_length = 10,default)
+    instituteId = models.TextField(max_length = 10)
     instituteName = models.TextField(blank = True, max_length = 50)
     instituteAddress = models.TextField(max_length = 50)
