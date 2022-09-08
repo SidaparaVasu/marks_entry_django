74470bdde29ce1a5996481919c8b8be1d4374980
codeonlife <bhavinkalal3118@gmail.com>
2022-09-08 09:50:25 +0530

    superadmin model created

--- superadmin/models.py
+++ superadmin/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Institute(models.Model):
+    instituteId = models.TextField(unique=True, max_length=11)
     instituteName = models.TextField(blank = True, max_length=50)
     instituteAddress = models.TextField(max_length=75)
