14250d58f3c5a8ef17fb49c5d31c1862d1ceb051
Yash992000 <imsc200047yash@ljku.edu.in>
2022-09-10 08:15:39 +0530

    institute
    insert data done

--- superadmin/models.py
+++ superadmin/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Institute(models.Model):
+    instituteID = models.IntegerField(unique=True)
     instituteName = models.TextField(blank = True, max_length=50)
-    instituteAddress = models.TextField(max_length=75)
+    instituteAddress = models.TextField(max_length=150)
