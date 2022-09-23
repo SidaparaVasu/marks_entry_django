1b6590ef515d4664146a9acaa0658c4da7afb47f
Sidapara_Vasu <74949446+SidaparaVasu@users.noreply.github.com>
2022-09-23 19:38:45 +0530

    set some fixes

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
  
 urlpatterns = [
     path('', views.adminIndex, name="adminIndex"), # admin index page
@@ -9,3 +12,6 @@ urlpatterns = [
     path('updateFaculty<id>', views.updateFaculty, name="updateFaculty"), 
     path('deleteFaculty<id>', views.deleteFaculty, name="deleteFaculty"), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- auth_app/urls.py
+++ auth_app/urls.py
@@ -7,9 +7,3 @@ urlpatterns = [
     path('registration', views.registration, name="registration"),
     path('loginHandle', views.loginHandle, name="loginHandle"),
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
