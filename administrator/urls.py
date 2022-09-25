7e5994e483ca4ba84ae8fd1e52134274b9c60f5c
Sidapara_Vasu <74949446+SidaparaVasu@users.noreply.github.com>
2022-09-25 21:36:08 +0530

    .

--- administrator/urls.py
+++ administrator/urls.py
@@ -6,11 +6,28 @@ from django.conf.urls.static import static
  
 urlpatterns = [
     path('', views.adminIndex, name="adminIndex"), # admin index page
+    
+    # faculty
     path('faculty',views.faculty,name="faculty"),
     path('addFaculty', views.addFaculty, name="addFaculty"),
     path('editFaculty/<id>', views.editFaculty, name="editFaculty"),
     path('updateFaculty<id>', views.updateFaculty, name="updateFaculty"), 
     path('deleteFaculty<id>', views.deleteFaculty, name="deleteFaculty"), 
+    
+    # batch
+    path('batch',views.batch,name="batch"),
+    path('addBatch', views.addBatch, name="addBatch"),
+    
+
+    # Semester
+    path('semester',views.semester,name="semester"),
+    path('addSemester', views.addSemester, name="addSemester"),
+    path('updateSemester<id>', views.updateSemester, name="updateSemester"), 
+
+    #Subject
+    path('subject',views.subject,name="subject"),
+    path('addSubject', views.addSubject, name="addSubject"),
+    
 ]
 
 if settings.DEBUG:
